141a0c884a0cd7019e176a866499081f9e051f3b
John S <tinco7@aol.com>
2019-10-30 21:06:18 -0700

    working mostly. Need to show search progress.
    Need to display search progressively and display whats going on.

--- draw-stuff.js
+++ draw-stuff.js
@@ -1,10 +1,6 @@
-var nodes = [];
-
 // The `links` array contains objects with a `source` and a `target`
 // property. The values of those properties are the indices in
 // the `nodes` array of the two endpoints of the link.
-
-var links = [];
 var maxone = 15;
 var maxtwo = 8;
 var maxthree = 7;
@@ -15,8 +11,65 @@ var destNode1 = 5;
 var destNode2 = 5;
 var destNode3 = 5;
 var myArr = createArray(15, 8, 7);
+function drawTxt(ctx,x,y,z){
+  var xtxt;
+  switch(x){
+    case 0:
+    xtxt='0';
+    break;
+    case 1:
+    xtxt='1';
+    break;
+    case 2:
+    xtxt='2';
+    break;
+    case 3:
+    xtxt='3';
+    break;
+    case 4:
+    xtxt='4';
+    break;
+    case 5:
+    xtxt='5';
+    break;
+    case 6:
+    xtxt='6';
+    break;
+    case 7:
+    xtxt='7';
+    break;
+    case 8:
+    xtxt='8';
+    break;
+    case 9:
+    xtxt='9';
+    break;
+    case 10:
+    xtxt='A';
+    break;
+    case 11:
+    xtxt='B';
+    break;
+    case 12:
+    xtxt='C';
+    break;
+    case 13:
+    xtxt='D';
+    break;
+    case 14:
+    xtxt='E';
+    break;
+    case 15:
+    xtxt='F';
+    break;
+  }
 
-function draw_rect(ctx, stroke,fill, x, y)
+  ctx.save( );
+  context.fillStyle = 'white';
+  ctx.fillText( xtxt + y + z, x*40+20+z*40, y*40+20+z*40-4);
+  ctx.restore( );
+}
+async function draw_room(ctx, stroke,fill, x, y, z)
 {
     stroke = stroke || 'lightgrey';
     fill = fill || 'dimgrey';
@@ -24,66 +77,20 @@ function draw_rect(ctx, stroke,fill, x, y)
     ctx.strokeStyle = stroke;
     ctx.fillStyle = fill;
     ctx.lineWidth = 5;
-    ctx.rect(x, y, 10, 10);
-    ctx.stroke();
-    ctx.fill();
+    ctx.rect(x*40+20+z*40, y*40+20+z*40, 5, 5);
+    //ctx.stroke();
+    ctx.fillRect(x*40+20+z*40, y*40+20+z*40, 5, 5);
     ctx.restore( );
 }
-function drawLine(ctx, stroke, x1, y1, x2,y2){
+function drawLine(ctx, stroke, x1, y1,z1, x2,y2,z2){
     stroke = stroke || 'blue';
     ctx.save( );
     ctx.beginPath();
     ctx.strokeStyle = stroke;
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(-x2, -y2);
+    ctx.moveTo(x1*40+20+z1*40, y1*40+20+z1*40);
+    ctx.lineTo(x2*40+20+z2*40, y2*40+20+z2*40);
     ctx.stroke();
     ctx.restore();
-}
-function drawSomething(){
-  var height = 600;
-  links.forEach(function(link){
-    links.source = nodes[link.source] ||
-    (nodes[link.source] = {name: link.source});
-    link.target = nodes[link.target] ||
-    (nodes[link.target] = {name: link.target});
-  });
-  var svg = d3.select('body').append('svg')
-  .attr('width', height).attr('height', height);
-  var force = d3.layout.force()
-  .size([height, height])
-  .nodes(d3.values(nodes))
-  .links(links)
-  .on("tick", tick)
-  .linkDistance(400)
-  .start();
-  var link = svg.selectAll('.link')
-  .data(links)
-  .enter().append('line')
-  .attr('class','link');
-  var node = svg.selectAll('.node')
-  .data(force.nodes())
-  .enter().append('circle')
-  .attr('class', 'node')
-  .attr("color", "red")
-  .attr('r', height*.01);
-  function tick(e){
-    node.attr('cx', function(d){return d.x})
-    .attr('cy', function(d){return d.y})
-    .call(force.drag);
-    link.attr('x1', function(d){return d.source.x;})
-    .attr('y1', function(d){return d.source.y;})
-    .attr('x2', function(d){return d.target.x;})
-    .attr('y2', function(d){return d.target.y;})
-  }
-}
-function updateGraph(){
-  svg.selectAll("circle")
-  .data(data)
-  .join(
-    enter => enter.append("circle").attr("fill", "green"),
-    update => update.attr("fill", "red")
-  )
-    .attr("stroke", "#8B0000");
 }
 function blackArray(){
   for (var i = 0; i < 15; i++){
@@ -100,15 +107,6 @@ var d = new Date();
 var d2 = null;
 do { d2 = new Date(); }
 while(d2-d < ms);
-}
-function fillArray(){
-  for (var i = 0; i < 15; i++){
-    for (var j = 0; j < 8; j++){
-      for (var k = 0; k < 7; k++){
-        nodes.push({x:i*30+10*k+10, y:j*30+10*k+10});
-      }
-    }
-  }
 }
 function createArray(length) {
     var arr = new Array(length || 0),
@@ -151,7 +149,12 @@ function colorNodes(one, two, three){
             if (i == one && k != three && j != two || i != one && k == three && j != two || i != one && k != three && j == two){
               if (i != maxone && i != 0){
                 if (i!=one){
-                  myArr[i][j][k] = "Candidate";
+                  if (myArr[i][j][k]!="red"){
+                    myArr[i][j][k] = "Candidate";
+                    draw_room(context, 'blue', 'blue', i, j, k);
+                    drawLine(context,'blue',previ,prevj,prevk,i,j,k);
+                    drawTxt(context, i, j,k);
+                  }
                   console.log("(" + i + ", " + j + ", " + k + ") " + myArr[i][j][k]);
                   res = Math.abs(i - j) + Math.abs(j - k) + Math.abs(k - i);
                   console.log("RESIDUE: " + res);
@@ -162,19 +165,23 @@ function colorNodes(one, two, three){
                     lowresj = j;
                     lowresk = k;
                     console.log("Moving to "+ "(" + lowresi + ", " + lowresj + ", " + lowresk + ")");
-                    nodes.push({x:i*10+10*k+10, y:j*10+10*k+10});
-                    links.push({ source: curr, target: curr+1 });
                     myArr[i][j][k] = "red";
-                    draw_rect(context, 'darkred', 'red', i*30+10*k+10, j*30+10*k+10);
+                    drawLine(context,'red',previ,prevj,prevk,i,j,k);
+                    draw_room(context, 'red', 'red', i, j, k);
+                    drawTxt(context, i, j,k);
                     curr++;
-                    //wait(500);
                     colorNodes(lowresi, lowresj, lowresk);
                   }
                 }
               }
               else if(j != maxtwo && j != 0){
                 if (i!=two){
-                  myArr[i][j][k] = "Candidate";
+                  if (myArr[i][j][k]!="red"){
+                    myArr[i][j][k] = "Candidate";
+                    draw_room(context, 'blue', 'blue', i, j, k);
+                    drawLine(context,'blue',previ,prevj,prevk,i,j,k);
+                    drawTxt(context, i, j,k);
+                  }
                   console.log("(" + i + ", " + j + ", " + k + ") " + myArr[i][j][k]);
                   res = Math.abs(i - j) + Math.abs(j - k) + Math.abs(k - i);
                   console.log("RESIDUE: " + res);
@@ -185,19 +192,23 @@ function colorNodes(one, two, three){
                     lowresj = j;
                     lowresk = k;
                     console.log("Moving to "+ "(" + lowresi + ", " + lowresj + ", " + lowresk + ")");
-                    nodes.push({x:i*10+10*k+10, y:j*10+10*k+10});
-                    links.push({ source: curr, target: curr+1 });
                     myArr[i][j][k] = "red";
-                    draw_rect(context, 'darkred', 'red', i*30+10*k+10, j*30+10*k+10);
+                    drawLine(context,'red',previ,prevj,prevk,i,j,k);
+                    draw_room(context, 'red', 'red', i, j, k);
+                    drawTxt(context, i, j,k);
                     curr++;
-                    //wait(500);
                     colorNodes(lowresi, lowresj, lowresk);
                   }
                 }
               }
               else if(k != maxthree && k != 0){
                 if (k!=three){
-                  myArr[i][j][k] = "Candidate";
+                  if (myArr[i][j][k]!="red"){
+                    myArr[i][j][k] = "Candidate";
+                    draw_room(context, 'blue', 'blue', i, j, k);
+                    drawLine(context,'blue',previ,prevj,prevk,i,j,k);
+                    drawTxt(context, i, j,k);
+                  }
                   console.log("(" + i + ", " + j + ", " + k + ") " + myArr[i][j][k]);
                   res = Math.abs(i - j) + Math.abs(j - k) + Math.abs(k - i);
                   console.log("RESIDUE: " + res);
@@ -208,12 +219,11 @@ function colorNodes(one, two, three){
                     lowresj = j;
                     lowresk = k;
                     console.log("Moving to "+ "(" + lowresi + ", " + lowresj + ", " + lowresk + ")");
-                    nodes.push({x:i*10+10*k+10, y:j*10+10*k+10});
-                    links.push({ source: curr, target: curr+1 });
                     myArr[i][j][k] = "red";
-                    draw_rect(context, 'darkred', 'red', i*30+10*k+10, j*30+10*k+10);
+                    drawLine(context,'red',previ,prevj,prevk,i,j,k);
+                    draw_room(context, 'red', 'red', i, j, k);
+                    drawTxt(context, i, j,k);
                     curr++;
-                    //wait(500);
                     colorNodes(lowresi, lowresj, lowresk);
                   }
                 }
@@ -226,18 +236,11 @@ function colorNodes(one, two, three){
   }
 }
 function myGraph(){
-
   for (var i = 0; i < 15; i++){
     for (var j = 0; j < 8; j++){
       for (var k = 0; k < 7; k++){
-        if (myArr[i][j][k] == "Candidate"){
-          draw_rect(context, i*10+10*k, j*10+10*k);
-          drawLine(context, i*10+10*k, j*10+10*k, i*10+10*k-5, j*10+10*k-5);
-        }
-        else if (myArr[i][j][k]=="red"){
-          draw_rect(context, 'darkred', 'red', i*10+10*k, j*10+10*k);
-          drawLine(context,'red', i*10+10*k, j*10+10*k, i*10+10*k-5, j*10+10*k-5);
-        }
+          draw_room(context, 'gray', 'gray', i, j, k);
+          //drawLine(context,'red', i*10+10*k, j*10+10*k, i*10+10*k-5, j*10+10*k-5);
       }
     }
   }
